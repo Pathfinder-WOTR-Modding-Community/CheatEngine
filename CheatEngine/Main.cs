738412bab4965c38731a121c66a3258acb268f5d
TylerGoeringer <tyler.goeringer@gmail.com>
2022-10-16 11:25:54 -0700

    Initial check-in

---
+++ CheatEngine/Main.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using static UnityModManagerNet.UnityModManager.ModEntry;
+using System;
+using UnityModManagerNet;
+using CheatEngine.Util;
+
+namespace CheatEngine
+{
+  public static class Main
+  {
+    private static readonly ModLogger Logger = Logging.GetLogger(nameof(Main));
+
+    public static bool Load(UnityModManager.ModEntry modEntry)
+    {
+      try
+      {
+        var harmony = new Harmony(modEntry.Info.Id);
+        harmony.PatchAll();
+
+        Logger.Log("Finished patching.");
+      }
+      catch (Exception e)
+      {
+        Logger.LogException("Failed to patch", e);
+      }
+      return true;
+    }
+  }
+}
---
+++ CheatEngine/Util/Logging.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using static UnityModManagerNet.UnityModManager.ModEntry;
+
+namespace CheatEngine.Util
+{
+  public static class Logging
+  {
+    private const string BaseChannel = "Cheats";
+
+    private static readonly Dictionary<string, ModLogger> Loggers = new();
+
+    public static ModLogger GetLogger(string channel)
+    {
+      if (Loggers.ContainsKey(channel))
+      {
+        return Loggers[channel];
+      }
+      var logger = new ModLogger($"{BaseChannel}+{channel}");
+      Loggers[channel] = logger;
+      return logger;
+    }
+  }
+}
